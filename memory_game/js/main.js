3e59d43599bce78e84787f0548001c3e380d6dd8
unknown <raymondlevengood@gmail.com>
2019-08-18 16:04:51 -0400

    v2 of game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,21 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
 var cardTwo = cards[2];
 cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardTwo);
-
-    if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] === cardsInPlay[1])  {
-            alert("You found a match!");
-        }   else { alert ("Sorry, Try again.");
-    }
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1])  {
+       console.log("You found a match!");
+    }   else { console.log("Sorry, Try again.");
 }
+}
+
+function flipCard(cardID) {
+    console.log("User flipped " + cards[cardID]);
+    cardsInPlay.push(cards[0]);
+}
+
+flipCard(0);
+flipCard(1);
+checkForMatch();
